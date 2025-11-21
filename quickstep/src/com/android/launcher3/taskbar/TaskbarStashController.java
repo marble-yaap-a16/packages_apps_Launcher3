73bed006ff74eaccfa810ea5dc9fc7590247cfac
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 13:21:43 +0200

    fixup! Launcher3: Implement gesture hint toggle
    Change-Id: I75b37a23996a4784320b45bccfb601c3042d22bf

--- quickstep/src/com/android/launcher3/taskbar/TaskbarStashController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarStashController.java
@@ -29,7 +29,7 @@ import static com.android.launcher3.config.FeatureFlags.ENABLE_TASKBAR_NAVBAR_UN
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_TRANSIENT_TASKBAR_HIDE;
 import static com.android.launcher3.logging.StatsLogManager.LauncherEvent.LAUNCHER_TRANSIENT_TASKBAR_SHOW;
 import static com.android.launcher3.taskbar.TaskbarActivityContext.ENABLE_TASKBAR_BEHIND_SHADE;
-import static com.android.launcher3.taskbar.TaskbarManager.NAVIGATION_BAR_HINT;
+import static com.android.launcher3.taskbar.TaskbarManagerImpl.NAVIGATION_BAR_HINT;
 import static com.android.launcher3.util.Executors.UI_HELPER_EXECUTOR;
 import static com.android.launcher3.util.FlagDebugUtils.appendFlag;
 import static com.android.launcher3.util.FlagDebugUtils.formatFlagChange;
@@ -328,7 +328,7 @@ public class TaskbarStashController implements TaskbarControllers.LoggableTaskba
         } else {
             mUnstashedHeight = mActivity.getDeviceProfile().getTaskbarProfile().getHeight();
             mStashedHeight = SettingsCache.INSTANCE.get(mActivity).getValue(NAVIGATION_BAR_HINT, 1)
-                    ? mActivity.getDeviceProfile().getTaskbarProfile().getStashedTaskbarHeight();
+                    ? mActivity.getDeviceProfile().getTaskbarProfile().getStashedTaskbarHeight()
                     : 0;
         }
     }
